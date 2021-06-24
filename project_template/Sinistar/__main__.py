cdd1a1ba37e6a3874ca469582238008b91fd4d24
drbubbles40-school <82907102+drbubbles40-school@users.noreply.github.com>
2021-06-24 15:03:27 -0600

    added code to main, added sinistarwindow

--- project_template/Sinistar/__main__.py
+++ project_template/Sinistar/__main__.py
@@ -1,1 +1,13 @@
 # program entry point
+from data.sinistarwindow import SinistarWindow
+import arcade
+from data import constants
+def main():
+
+    window = SinistarWindow(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, constants.SCREEN_TITLE)
+
+    window.setup()
+    window.run()
+
+if __name__ == "__main__":
+    main()

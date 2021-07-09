99f03aa5ef7637cce030d4642bc7aa9eaf306536
Helix088 <pat20001@byui.edu>
2021-07-09 18:44:18 -0500

    difficulty and split_asteroid

--- project_template/Sinistar/data/asteroid_manager.py
+++ project_template/Sinistar/data/asteroid_manager.py
@@ -1,40 +1,42 @@
-import arcade, random, math
+import arcade
+import random
+import math
 from data import constants
 from data.asteroid import Asteroid
+
 
 class AsteroidManager(arcade.SpriteList):
     """This class manages the asteroid objects for use in Sinistar window"""
 
     def __init__(self, player_sprite):
         """Generate initial list of asteroids
-        
+
         Args:
             self - an instance of AsteroidManager
         """
         super().__init__()
         self._generate_list(player_sprite)
-        
 
     def _generate_list(self, player_sprite):
         """Fills self with asteroid objects
-        
+
         Args:
             self - instance of AsteroidManager
         """
         for _ in range(constants.ASTEROID_COUNT):
-            #Set Position
+            # Set Position
             asteroid = Asteroid(player_sprite)
             self.append(asteroid)
 
     def respawn_asteroids(self, player_sprite, all_sprites):
         """Checks if asteroids need to be respawned, 
         if so it has a chance of respawning
-        
+
         Args:
             self - instance of AsteroidManager
             all_sprites - List of all sprites
         """
-        count =  constants.ASTEROID_COUNT
+        count = constants.ASTEROID_COUNT
         num_asteroids = len(self)
         if num_asteroids < count:
             odds = 200
@@ -44,3 +46,70 @@ class AsteroidManager(arcade.SpriteList):
                 asteroid = Asteroid(player_sprite)
                 self.append(asteroid)
                 all_sprites.append(asteroid)
+
+    def split_asteroid(self, asteroid: Asteroid):
+        """ Splits Asteroids into chunks when collision happens """
+        x = asteroid.center_x
+        y = asteroid.center_y
+        self._score += 1
+
+        if asteroid.size == 4:
+            for _ in range(3):
+                image_no = random.randrange(2)
+                image_list = []
+
+                enemy_sprite = Asteroid(
+                    image_list[image_no], self._constants.SPRITE_SCALING_ASTEROIDS * 1.5)
+                enemy_sprite.center_y = y
+                enemy_sprite.center_x = x
+
+                enemy_sprite.change_x = random.random() * 2.5 - 1.25
+                enemy_sprite.change_y = random.random() * 2.5 - 1.25
+
+                enemy_sprite.change_angle = (random.random() - 0.5) * 2
+                enemy_sprite.size = 3
+
+                self.asteroid_list.append(enemy_sprite)
+                # sound goes here
+
+        elif asteroid.size == 3:
+            for _ in range(3):
+                image_no = random.randrange(2)
+                image_list = []
+
+                enemy_sprite = Asteroid(
+                    image_list[image_no], self._constants.SPRITE_SCALING_ASTEROIDS * 1.5)
+                enemy_sprite.center_y = y
+                enemy_sprite.center_x = x
+
+                enemy_sprite.change_x = random.random() * 3 - 1.5
+                enemy_sprite.change_y = random.random() * 3 - 1.5
+
+                enemy_sprite.change_angle = (random.random() - 0.5) * 2
+                enemy_sprite.size = 2
+
+                self.asteroid_list.append(enemy_sprite)
+                # sound goes here
+
+        elif asteroid.size == 2:
+            for _ in range(3):
+                image_no = random.randrange(2)
+                image_list = []
+
+                enemy_sprite = Asteroid(
+                    image_list[image_no], self._constants.SPRITE_SCALING_ASTEROIDS * 1.5)
+                enemy_sprite.center_y = y
+                enemy_sprite.center_x = x
+
+                enemy_sprite.change_x = random.random() * 3.5 - 1.75
+                enemy_sprite.change_y = random.random() * 3.5 - 1.75
+
+                enemy_sprite.change_angle = (random.random() - 0.5) * 2
+                enemy_sprite.size = 1
+
+                self.asteroid_list.append(enemy_sprite)
+                # sound goes here
+
+        elif asteroid.size == 1:
+            # sound goes here
+            self.asteroid
--- project_template/Sinistar/data/difficulty.py
+++ project_template/Sinistar/data/difficulty.py
@@ -3,6 +3,7 @@ from data import sinistarwindow
 from data import asteroid
 from data import enemies
 from data import menu
+from data import warrior
 
 
 class Difficulty():
@@ -13,24 +14,26 @@ class Difficulty():
     """
 
     def __init__(self):
-        self._difficulty = constants.NORMAL
+        self._enemy_difficulty = 3
+        self._asteroid_difficulty = 3
 
     def _set_difficulty(self):
         if self._menu._change_difficulty() == 1:
-            self._difficulty = self._asteroid.add_asteroid(
-                1) & self._enemies.retrieve_difficulty(1)
+            self._asteroid_difficulty = self._asteroid._
+            self._enemy_difficulty = self._enemies._retrieve_difficulty
+            self._difficulty = 1
         elif self._menu._change_difficulty() == 2:
-            self._difficulty = self._asteroid.add_asteroid(
-                2) & self._enemies.retrieve_difficulty(2)
+            self._enemies._retrieve_difficulty
+            self._difficulty = 2
         elif self._menu._change_difficulty() == 3:
-            self._difficulty = self._asteroid.add_asteroid(
-                3) & self._enemies.retrieve_difficulty(3)
+            self._enemies._retrieve_difficulty
+            self._difficulty = 3
         elif self._menu._change_difficulty() == 4:
-            self._difficulty = self._asteroid.add_asteroid(
-                4) & self._enemies.retrieve_difficulty(4)
+            self._enemies._retrieve_difficulty
+            self._difficulty = 4
         else:
-            self._difficulty = self._asteroid.add_asteroid(
-                5) & self._enemies.retrieve_difficulty(5)
+            self._enemies._retrieve_difficulty
+            self._difficulty = 5
 
     def _get_difficulty(self):
         """Returns the difficulty"""
